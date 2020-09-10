e222319bdd743099b934e60ee9637a4de4600696
Artem <artemon.kos@yandex.ru>
2020-09-10 09:45:18 +0300

    My homework #1

---
+++ CSharpLight/TenVariable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLight
+{
+    class TenVariable
+    {
+        // My Homework #1
+        float piNumber = 3.14f;
+        string myName = "Artem";
+        uint countMyFullAges = 20;
+        long countDaysILive = 7395;
+        bool decision = true;
+        byte numberOfProcessors = 8;
+        short lenghtOfMyName = 5;
+        char firstLetterMyName = 'A';
+        ushort ushortMax = 65535;
+        int minAllUnsigned = 0;3
+    }
+}
