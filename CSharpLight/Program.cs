174dd8cf09cc18a4cf5639736e280cb505c1d8a5
Artem <artemon.kos@yandex.ru>
2020-09-10 09:38:51 +0300

    Initialization VS

---
+++ CSharpLight/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
