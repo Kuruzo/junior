9fe8608eb562f95971404ffb95023ea46e5be2ae
Artem <artemon.kos@yandex.ru>
2020-09-10 12:21:27 +0300

    Homework #4

---
+++ CSharpLight/CristalShop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpLight
+{
+    public class CristalShop
+    {
+        public static void StartMainProgram()
+        {
+            Console.WriteLine("Welcome to <Cristal Shop>!");
+            
+            Console.WriteLine("How much Gold do you have?");
+            int countGold = Convert.ToInt32(Console.ReadLine());
+            
+            const int CristalPrice = 23;
+            Console.WriteLine($"Good! Cristal cost {CristalPrice} Gold. How much do you want?");
+            int cristalOrder = Convert.ToInt32(Console.ReadLine());
+
+            int countSellCristal = 0;
+            for (int i = 1; i <= cristalOrder && countGold >= CristalPrice; i++)
+            {
+                countSellCristal++;
+                countGold -= CristalPrice;
+            }
+
+            Console.WriteLine($"Your Gold is enough for {countSellCristal} Cristal.");
+            Console.WriteLine("Thanks for your purchase! Good bye!");
+            Console.WriteLine();
+            Console.WriteLine($"Hmm...I have {countGold} Gold and {countSellCristal} Cristal.");
+        }
+    }
+}
