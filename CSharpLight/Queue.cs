ddbafc312e819b96084db5cdddaa9da80f432e08
Artem <artemon.kos@yandex.ru>
2020-09-21 15:10:22 +0300

    Homework #5

---
+++ CSharpLight/Queue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLight
+{
+    class Queue
+    {
+        public static void StartMainMettod()
+        {
+            Console.WriteLine("Wellcome to <Queue>!");
+            Console.WriteLine("How much women you see: ");
+            int womenInQueue = Convert.ToInt32(Console.ReadLine());
+
+            const int timeForOneWoman = 10;
+            int fullTime = womenInQueue * timeForOneWoman;
+
+            Console.WriteLine($"You have to wait: {fullTime / 60} hours and {fullTime % 60} minets.");
+        }
+    }
+}
