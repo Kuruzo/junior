034b06d96c9442422bdd1eff0d496d55ef27778d
Artem <artemon.kos@yandex.ru>
2020-09-23 22:10:01 +0300

    Init Codewars Folder and make first program <Digital Root>

--- SolutionFromCodewars/Kata_6kyu.cs
+++ SolutionFromCodewars/Kata_6kyu.cs
@@ -1,21 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.Remoting.Messaging;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharpLight
+namespace SolutionFromCodewars
 {
-    class Program
+    public static class Kata_6kyu
     {
-        static void Main(string[] args)
-        {
-            // Queue.StartMainMettod();
-            Console.WriteLine("Hello!");
-            Console.WriteLine(DigitalRoot(123));
-        }
         public static int DigitalRoot(long n)
         {
             int result = 0;
@@ -30,5 +20,5 @@ namespace CSharpLight
             }
             else return result;
         }
-    }  
+    }
 }
