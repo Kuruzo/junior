22a33ea0f27b1ccc5459d4673f05243255719a9d
Kuruzo <artemon.kos@yandex.ru>
2020-10-18 11:08:05 +0200

    Remake Homework #5

--- CSharpLight/Queue.cs
+++ CSharpLight/Queue.cs
@@ -8,16 +8,18 @@ namespace CSharpLight
 {
     class Queue
     {
-        public static void StartMainMettod()
+        public static void Run()
         {
             Console.WriteLine("Wellcome to <Queue>!");
             Console.WriteLine("How much women you see: ");
             int womenInQueue = Convert.ToInt32(Console.ReadLine());
 
-            const int timeForOneWoman = 10;
-            int fullTime = womenInQueue * timeForOneWoman;
+            const short timeForOneWoman = 10;
+            var fullTime = womenInQueue * timeForOneWoman;
+            var hours = fullTime / 60;
+            var minets = fullTime % 60;
 
-            Console.WriteLine($"You have to wait: {fullTime / 60} hours and {fullTime % 60} minets.");
+            Console.WriteLine($"You have to wait: {hours} hour(s) and {minets} minet(s).");
         }
     }
 }
