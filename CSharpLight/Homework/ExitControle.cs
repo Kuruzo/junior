c0833cecf378a227cd971f738e6958dcb35fba99
Kuruzo <artemon.kos@yandex.ru>
2020-10-18 11:37:40 +0200

    Homework #8

---
+++ CSharpLight/Homework/ExitControle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLight
+{
+    class ExitControle
+    {
+        public static void Run()
+        {
+            Console.WriteLine("You can exit only writing \"exit\"");
+            while (true)
+            {
+                if (Console.ReadLine() == "exit") break;
+            }
+        }
+    }
+}
